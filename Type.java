adbfd479336e2959346356ecf7c043a2735d6e75
Eunhee-nim <126763448+Eunhee-nim@users.noreply.github.com>
2023-04-04 09:21:46 +0900

    입력한 값을 StringBuilder 로 받아 형변환 후 출력

--- Type.java
+++ Type.java
@@ -1,57 +1,34 @@
-package homework;
+package day0403t;
 
 import java.util.Scanner;
 
 public class Type {
-
+//사용자가 입력한 수(실수나 정수 모두 가능)를 StringBuilder로 받아서 
+//점(.)이 포함되면 double형으로 포함되지 않으면 int형으로 변경 후 출력하세요.
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		//사용자가 두 개의 정수를 입력하면 문자열로 받아서 
-		//두 개의 문자열 정수를 정수형으로 변환해서 합을 출력하세요. 
-		System.out.println("두 개의 정수를 입력하세요.");
-		String num = sc.next();
-		String num1 = sc.next();
+		System.out.println("실수나 정수를 입력해주세요.");
 		
-		System.out.println(Integer.parseInt(num)+Integer.parseInt(num1));
+		String input = sc.nextLine(); //String으로 입력 받음
+		StringBuilder sb = new StringBuilder(input); //StringBuilder 생성
+		double douVal = 0; //double 값 만들기
+		int intVal =0; //int값 만들기
+		String str = null;
 		
-		System.out.println("두 개의 정수를 입력하세요");
-		System.out.println("a :");
-		int a = sc.nextInt();
-		System.out.println("b :");
-		int b = sc.nextInt();
-		
-		if (b>0) {
-			double div = a/b;
-			System.out.println(div);
-		} else if (b == 0) {
-			System.out.println("잘못 입력하셨습니다. b는 0 이상의 수를 입력해주세요.");
+		for (int i = 0; i < input.length() ; i++) {
+			if (sb.indexOf(".")!=-1) { //indexOf는 
+				douVal = Double.parseDouble(input);
+				str = "double 형 변환" + douVal;
+			} else {
+				intVal = Integer.parseInt(input);
+				str = "int 형 변환" + intVal;
+				//str = for문 안에 실수나 정수가 들어갔을 때
+				//조건에 맞는 것만 삽입되게 하는 String 값
+			}
 		}
-		
-		//사용자가 실수 한 개를 입력하면 정수형으로 변환해서 출력하세요. (3~4)함께
-		//사용자가 한 개의 실수를 입력하면 그 실수의 제곱의 결과를 문자열로 변환하여 출력하세요.
-		System.out.println("실수를 입력하세요.");
-		float c = sc.nextFloat();
-		
-		System.out.println("실수의 정수형은 " + (int)c);
-		System.out.println("실수의 제곱은 " + String.valueOf(c*c));
-		
-		//5. int형 변수 iNum = 10과 long형 변수 lNum = 20의 값을 서로 교환하세요.
-		int iNum = 10;
-		long lNum = 20;
-		
-		int x = iNum;
-		iNum = (int)lNum;
-		lNum = x;
-		
-		//6.사용자가 입력한 두 개의 문자 각각의 유니코드를 출력하세요.
-		System.out.println("두 개의 문자를 입력하세요.");
-		char d = sc.next().charAt(0);
-		char e = sc.next().charAt(0);
-		
-		System.out.println((int)d + " , " + (int)e);
-		
-		
+
+		System.out.println(str); //str값만 출력
 	}
 
 }
